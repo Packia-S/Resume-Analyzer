ac7e6b0e883f3d81be4bf8e18c6baeefe62a4b18
Packialakshmi <spackialakshmi1999@gmail.com>
2025-11-17 11:24:48 +0530

    Update main_pro.py

--- main_pro.py
+++ main_pro.py
@@ -118,7 +118,6 @@ with tab1:
                     temp_path,
                     export_type=ExportType.MARKDOWN,
                     pipeline_options={"do_ocr": False}
-
                 )
                 docs = loader.load()
                 resume_markdown = docs[0].page_content
@@ -427,6 +426,7 @@ with tab2:
 
 #                 st.success("Your data has been submitted successfully.")
 #                 st.rerun()
+
 
 
 
